12b5ede033aac5f308a37488b371041538c0680a
Lykrast <aymericpm@gmail.com>
2025-09-10 19:06:58 +0200

    Change potion effect bullet tooltips to be like tipped arrow

--- src/main/java/lykrast/gunswithoutrosesadditions/item/EffectBullet.java
+++ src/main/java/lykrast/gunswithoutrosesadditions/item/EffectBullet.java
@@ -5,21 +5,24 @@ import java.util.function.Supplier;
 
 import javax.annotation.Nullable;
 
+import com.google.common.collect.ImmutableList;
+
 import lykrast.gunswithoutroses.entity.BulletEntity;
 import lykrast.gunswithoutroses.item.BulletItem;
-import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.alchemy.PotionUtils;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class EffectBullet extends BulletItem {
 	private Supplier<MobEffectInstance> effect;
+	private List<MobEffectInstance> tooltipEffect;
 	
 	public EffectBullet(Properties properties, int damage, Supplier<MobEffectInstance> effect) {
 		super(properties, damage);
@@ -36,7 +39,8 @@ public class EffectBullet extends BulletItem {
 	@OnlyIn(Dist.CLIENT)
 	public void appendHoverText(ItemStack stack, @Nullable Level worldIn, List<Component> tooltip, TooltipFlag flagIn) {
 		super.appendHoverText(stack, worldIn, tooltip, flagIn);
-		tooltip.add(Component.translatable(getDescriptionId(stack) + ".desc").withStyle(ChatFormatting.GRAY));
+		if (tooltipEffect == null) tooltipEffect = ImmutableList.of(effect.get());
+		PotionUtils.addPotionTooltip(tooltipEffect, tooltip, 1);
 	}
 
 }
