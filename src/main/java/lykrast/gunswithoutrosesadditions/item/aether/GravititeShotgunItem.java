3fff762f20045fb38a063e0e1460591e9024148e
Lykrast <aymericpm@gmail.com>
2025-09-18 23:32:38 +0200

    Knockback on gravitite shotgun

---
+++ src/main/java/lykrast/gunswithoutrosesadditions/item/aether/GravititeShotgunItem.java
@@ -0,0 +1,66 @@
+package lykrast.gunswithoutrosesadditions.item.aether;
+
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import lykrast.gunswithoutroses.item.GunItem;
+import lykrast.gunswithoutroses.item.IBullet;
+import lykrast.gunswithoutrosesadditions.registry.CompatAether;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+
+public class GravititeShotgunItem extends GunItem {
+	private static final double RANGE = 4, RANGESQR = RANGE*RANGE, KNOCKBACK = 2;
+
+	public GravititeShotgunItem(Properties properties, int bonusDamage, double damageMultiplier, int fireDelay, double inaccuracy, int enchantability) {
+		super(properties, bonusDamage, damageMultiplier, fireDelay, inaccuracy, enchantability);
+	}
+	
+	@Override
+	protected void shoot(Level world, Player player, ItemStack gun, ItemStack ammo, IBullet bulletItem, boolean bulletFree) {
+		knocbackBurst(player, world);
+		super.shoot(world, player, gun, ammo, bulletItem, bulletFree);
+	}
+	
+
+	@Override
+	public void shootAt(LivingEntity shooter, double x, double y, double z, ItemStack gun, ItemStack ammo, IBullet bulletItem, double mobSpread, boolean bulletFree) {
+		knocbackBurst(shooter, shooter.level());
+		super.shootAt(shooter, x, y, z, gun, ammo, bulletItem, mobSpread, bulletFree);
+	}
+	
+	protected void knocbackBurst(LivingEntity shooter, Level level) {
+		//TODO particles
+		var origin = shooter.getEyePosition();
+		var look = shooter.getLookAngle();
+		for (LivingEntity ent : level.getEntitiesOfClass(LivingEntity.class, shooter.getBoundingBox().inflate(RANGE))) {
+			//immunity check
+			if (ent.getType().is(CompatAether.UNLAUNCHABLE)) continue;
+			//distance check so we actually a sphere
+			double distSqr = ent.distanceToSqr(origin);
+			if (distSqr > RANGESQR) continue;
+			//angle check, if I'm mathing right dot product of 0 is 90° to either side, and negative is behind, so that should be a half sphere
+			//plus a lil sphere for what's adjacent for leniency
+			var toTarget = ent.position().subtract(origin).normalize();
+			if (distSqr > 1 && toTarget.dot(look) < 0) continue;
+			//and now we actually knock back
+			double amplitude = KNOCKBACK*((distSqr / RANGESQR)*0.6 + 0.4);
+			amplitude *= Math.max(0, 1 - ent.getAttributeValue(Attributes.KNOCKBACK_RESISTANCE));
+			ent.push(toTarget.x*amplitude,(Math.max(0,toTarget.y)+0.2)*amplitude,toTarget.z*amplitude);
+			
+		}
+	}
+
+	@Override
+	protected void addExtraStatsTooltip(ItemStack stack, @Nullable Level world, List<Component> tooltip) {
+		super.addExtraStatsTooltip(stack, world, tooltip);
+		tooltip.add(Component.translatable("item.gunswithoutrosesadditions.gravitite_shotgun.desc").withStyle(ChatFormatting.GRAY));
+	}
+
+}
--- src/main/java/lykrast/gunswithoutrosesadditions/registry/CompatAether.java
+++ src/main/java/lykrast/gunswithoutrosesadditions/registry/CompatAether.java
@@ -1,11 +1,12 @@
 package lykrast.gunswithoutrosesadditions.registry;
 
-import lykrast.gunswithoutroses.item.GunItem;
 import lykrast.gunswithoutroses.registry.GWRSounds;
 import lykrast.gunswithoutrosesadditions.CompatModids;
 import lykrast.gunswithoutrosesadditions.entity.aether.AetherBulletEntity;
 import lykrast.gunswithoutrosesadditions.entity.aether.AetherBulletItem;
+import lykrast.gunswithoutrosesadditions.item.aether.GravititeShotgunItem;
 import lykrast.gunswithoutrosesadditions.item.aether.ZaniteGunItem;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
@@ -22,14 +23,15 @@ public class CompatAether {
 			GRAVITITE_REPAIR = ItemTags.create(new ResourceLocation(CompatModids.AETHER, "gravitite_repairing"));
 	
 	public static RegistryObject<EntityType<AetherBulletEntity>> aetherBullet;
+	public static TagKey<EntityType<?>> UNLAUNCHABLE = TagKey.create(Registries.ENTITY_TYPE, new ResourceLocation(CompatModids.AETHER, "unlaunchable"));
 	
 	public static void registerItems() {
 		//zanite has same durability as iron
 		//aether gun have higher accuracy lower fire rate, and the zanite bonus damage will kick in
 		zaniteGun = GWRAItems.initItem(() -> new ZaniteGunItem(GWRAItems.defP().durability(513), 0, 1, 18, 1, 14).repair(() -> Ingredient.of(ZANITE_REPAIR)), "zanite_gun");
 		//gravitite has durability of diamond
-		//TODO real stat and the knockback, entity type tag aether:unlaunchable for immune
-		gravititeShotgun = GWRAItems.initItem(() -> new GunItem(GWRAItems.defP().durability(2076), 0, 0.6, 20, 6, 10).projectiles(4).fireSound(GWRSounds.shotgun).repair(() -> Ingredient.of(GRAVITITE_REPAIR)), "gravitite_shotgun");
+		//TODO proper stats
+		gravititeShotgun = GWRAItems.initItem(() -> new GravititeShotgunItem(GWRAItems.defP().durability(2076), 0, 0.6, 20, 6, 10).projectiles(4).fireSound(GWRSounds.shotgun).repair(() -> Ingredient.of(GRAVITITE_REPAIR)), "gravitite_shotgun");
 		
 		zaniteBullet = GWRAItems.initItem(() -> new AetherBulletItem(GWRAItems.defP(), 6), "zanite_bullet");
 	}
